041bc065bd00dd59f6783a5a018477def233ca49
Mike Thompson <mithomps@redhat.com>
2015-01-26 09:37:26 -0800

    [HAWKULAR-3] - Create and show a "Hello World" hawt.io plugin.

--- ui/console/metrics/dist/hawkular-metrics.js
+++ ui/console/metrics/dist/hawkular-metrics.js
@@ -30,8 +30,8 @@ var HawkularMetrics;
 var HawkularMetrics;
 (function (HawkularMetrics) {
     HawkularMetrics.MetricsController = HawkularMetrics._module.controller("HawkularMetrics.MetricsController", ['$scope', function ($scope) {
-        $scope.target = "World!";
+        $scope.searchId = "";
     }]);
 })(HawkularMetrics || (HawkularMetrics = {}));
 
-angular.module("hawkular-metrics-templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("plugins/metrics/html/graphs.html","<div class=\"row\">\n    <div class=\"col-md-12\">\n       <h1>Hawkular Metrics Graphs</h1>\n    </div>\n    <div class=\"col-md-12\" ng-controller=\"HawkularMetrics.MetricsController\">\n        <p>Hello {{target}}</p>\n    </div>\n</div>\n");}]); hawtioPluginLoader.addModule("hawkular-metrics-templates");
+angular.module("hawkular-metrics-templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("plugins/metrics/html/graphs.html","<div class=\"row\" ng-controller=\"HawkularMetrics.MetricsController\">\n    <div class=\"col-md-12\">\n        <h1>Hawkular Metrics Graphs</h1>\n    </div>\n    <div class=\"col-md-8\" ng-controller=\"HawkularMetrics.MetricsController\">\n        <form class=\"form-horizontal\" name=\"chartForm\" role=\"form\" novalidate>\n\n            <div class=\"form-group\">\n                <label class=\"col-sm-2 control-label\">ID</label>\n\n                <div class=\"col-sm-5\">\n                    <input type=\"text\" class=\"form-control\" name=\"searchId\" ng-model=\"searchId\"\n                           placeholder=\"Enter Id...\" required ng-minlength=\"1\">\n                        <span class=\"error-message\"\n                              ng-show=\"chartForm.searchId.$dirty && chartForm.searchId.$error.required\"> * Required.</span>\n                    <span class=\"help-block\">Example: 100, apache3.cpu1  </span>\n                </div>\n            </div>\n        </form>\n\n    </div>\n\n</div>\n");}]); hawtioPluginLoader.addModule("hawkular-metrics-templates");
