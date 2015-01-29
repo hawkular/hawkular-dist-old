6f88a6eeb7d69e8816802173dc1ada8b5919298c
Mike Thompson <mithomps@redhat.com>
2015-01-29 13:41:02 -0800

    Delete the plugin example from UI console. Any plugin code should be added to hawkular-ui-components and pulled into this console via bower package.

--- ui/console/defs.d.ts
+++ ui/console/defs.d.ts
@@ -15,6 +15,3 @@
  * limitations under the License.
  */
 /// <reference path="d.ts/includes.d.ts"/>
-/// <reference path="d.ts/example/ts/exampleGlobals.d.ts"/>
-/// <reference path="d.ts/example/ts/examplePlugin.d.ts"/>
-/// <reference path="d.ts/example/ts/page1.d.ts"/>
--- ui/console/dist/hawkular-console.js
+++ ui/console/dist/hawkular-console.js
@@ -15,39 +15,3 @@
  * limitations under the License.
  */
 /// <reference path="../libs/hawtio-utilities/defs.d.ts"/>
-
-/// <reference path="../../includes.ts"/>
-var Example;
-(function (Example) {
-    Example.pluginName = "hawtio-assembly";
-    Example.log = Logger.get(Example.pluginName);
-    Example.templatePath = "plugins/example/html";
-})(Example || (Example = {}));
-
-/// <reference path="../../includes.ts"/>
-/// <reference path="exampleGlobals.ts"/>
-var Example;
-(function (Example) {
-    Example._module = angular.module(Example.pluginName, []);
-    var tab = undefined;
-    Example._module.config(['$locationProvider', '$routeProvider', 'HawtioNavBuilderProvider', function ($locationProvider, $routeProvider, builder) {
-        tab = builder.create().id(Example.pluginName).title(function () { return "Example"; }).href(function () { return "/example"; }).subPath("Page 1", "page1", builder.join(Example.templatePath, 'page1.html')).build();
-        builder.configureRouting($routeProvider, tab);
-        $locationProvider.html5Mode(true);
-    }]);
-    Example._module.run(['HawtioNav', function (HawtioNav) {
-        HawtioNav.add(tab);
-        Example.log.debug("loaded");
-    }]);
-    hawtioPluginLoader.addModule(Example.pluginName);
-})(Example || (Example = {}));
-
-/// <reference path="examplePlugin.ts"/>
-var Example;
-(function (Example) {
-    Example.Page1Controller = Example._module.controller("Example.Page1Controller", ['$scope', function ($scope) {
-        $scope.target = "World!";
-    }]);
-})(Example || (Example = {}));
-
-angular.module("hawkular-console-templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("plugins/example/html/page1.html","<div class=\"row\">\n  <div class=\"col-md-12\" ng-controller=\"Example.Page1Controller\">\n    <h1>Page 1</h1>\n    <p>Hello {{target}}</p>\n  </div>\n</div>\n");}]); hawtioPluginLoader.addModule("hawkular-console-templates");
