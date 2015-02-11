520928629976ddaeb0ac0e92bd722dc35d722b48
Mike Thompson <mithomps@redhat.com>
2015-02-10 18:02:27 -0800

    Add gulp-size to build to show uncompressed and gzipped sizes of build in console without having to determine in browser.

--- ui/console/gulpfile.js
+++ ui/console/gulpfile.js
@@ -21,133 +21,144 @@ var gulp = require('gulp'),
     map = require('vinyl-map'),
     fs = require('fs'),
     path = require('path'),
-    s = require('underscore.string');
+    size = require('gulp-size'),
+    s = require('underscore.string'),
     tslint = require('gulp-tslint');
 
 var plugins = gulpLoadPlugins({});
 var pkg = require('./package.json');
-
-var config = {
-  main: '.',
-  ts: ['plugins/**/*.ts'],
-  templates: ['plugins/**/*.html'],
-  templateModule: pkg.name + '-templates',
-  dist: './dist/',
-  js: pkg.name + '.js',
-  tsProject: plugins.typescript.createProject({
-    target: 'ES5',
-    module: 'commonjs',
-    declarationFiles: true,
-    noExternalResolve: false
-  })
+var normalSizeOptions = {
+    showFiles: true
+}, gZippedSizeOptions = {
+    showFiles: true,
+    gzip: true
 };
 
-gulp.task('bower', function() {
-  gulp.src('index.html')
-    .pipe(wiredep({}))
-    .pipe(gulp.dest('.'));
+var config = {
+    main: '.',
+    ts: ['plugins/**/*.ts'],
+    templates: ['plugins/**/*.html'],
+    templateModule: pkg.name + '-templates',
+    dist: './dist/',
+    js: pkg.name + '.js',
+    tsProject: plugins.typescript.createProject({
+        target: 'ES5',
+        module: 'commonjs',
+        declarationFiles: true,
+        noExternalResolve: false
+    })
+};
+
+gulp.task('bower', function () {
+    gulp.src('index.html')
+        .pipe(wiredep({}))
+        .pipe(gulp.dest('.'));
 });
 
 /** Adjust the reference path of any typescript-built plugin this project depends on */
-gulp.task('path-adjust', function() {
-  gulp.src('libs/**/includes.d.ts')
-    .pipe(map(function(buf, filename) {
-      var textContent = buf.toString();
-      var newTextContent = textContent.replace(/"\.\.\/libs/gm, '"../../../libs');
-      // console.log("Filename: ", filename, " old: ", textContent, " new:", newTextContent);
-      return newTextContent;
-    }))
-    .pipe(gulp.dest('libs'));
+gulp.task('path-adjust', function () {
+    gulp.src('libs/**/includes.d.ts')
+        .pipe(map(function (buf, filename) {
+            var textContent = buf.toString();
+            var newTextContent = textContent.replace(/"\.\.\/libs/gm, '"../../../libs');
+            // console.log("Filename: ", filename, " old: ", textContent, " new:", newTextContent);
+            return newTextContent;
+        }))
+        .pipe(gulp.dest('libs'));
 });
 
-gulp.task('clean-defs', function() {
-  return gulp.src('defs.d.ts', { read: false })
-    .pipe(plugins.clean());
+gulp.task('clean-defs', function () {
+    return gulp.src('defs.d.ts', {read: false})
+        .pipe(plugins.clean());
 });
 
-gulp.task('tsc', ['clean-defs'], function() {
-  var cwd = process.cwd();
-  var tsResult = gulp.src(config.ts)
-    .pipe(plugins.typescript(config.tsProject))
-    .on('error', plugins.notify.onError({
-      message: '#{ error.message }',
-      title: 'Typescript compilation error'
-    }));
+gulp.task('tsc', ['clean-defs'], function () {
+    var cwd = process.cwd();
+    var tsResult = gulp.src(config.ts)
+        .pipe(plugins.typescript(config.tsProject))
+        .on('error', plugins.notify.onError({
+            message: '#{ error.message }',
+            title: 'Typescript compilation error'
+        }));
 
     return eventStream.merge(
-      tsResult.js
-        .pipe(plugins.concat('compiled.js'))
-        .pipe(gulp.dest('.')),
-      tsResult.dts
-        .pipe(gulp.dest('d.ts')))
-        .pipe(map(function(buf, filename) {
-          if (!s.endsWith(filename, 'd.ts')) {
+        tsResult.js
+            .pipe(plugins.concat('compiled.js'))
+            .pipe(gulp.dest('.')),
+        tsResult.dts
+            .pipe(gulp.dest('d.ts')))
+        .pipe(map(function (buf, filename) {
+            if (!s.endsWith(filename, 'd.ts')) {
+                return buf;
+            }
+            var relative = path.relative(cwd, filename);
+            fs.appendFileSync('defs.d.ts', '/// <reference path="' + relative + '"/>\n');
             return buf;
-          }
-          var relative = path.relative(cwd, filename);
-          fs.appendFileSync('defs.d.ts', '/// <reference path="' + relative + '"/>\n');
-          return buf;
         }));
 });
 
-gulp.task('tslint', function(){
-  gulp.src(config.ts)
-    .pipe(tslint())
-    .pipe(tslint.report('verbose'));
+gulp.task('tslint', function () {
+    gulp.src(config.ts)
+        .pipe(tslint())
+        .pipe(tslint.report('verbose'));
 });
 
-gulp.task('tslint-watch', function(){
-  gulp.src(config.ts)
-    .pipe(tslint())
-    .pipe(tslint.report('prose', {
-      emitError: false
-    }));
+gulp.task('tslint-watch', function () {
+    gulp.src(config.ts)
+        .pipe(tslint())
+        .pipe(tslint.report('prose', {
+            emitError: false
+        }));
 });
 
-gulp.task('template', ['tsc'], function() {
-  return gulp.src(config.templates)
-    .pipe(plugins.angularTemplatecache({
-      filename: 'templates.js',
-      root: 'plugins/',
-      standalone: true,
-      module: config.templateModule,
-      templateFooter: '}]); hawtioPluginLoader.addModule("' + config.templateModule + '");'
-    }))
-    .pipe(gulp.dest('.'));
+gulp.task('template', ['tsc'], function () {
+    return gulp.src(config.templates)
+        .pipe(plugins.angularTemplatecache({
+            filename: 'templates.js',
+            root: 'plugins/',
+            standalone: true,
+            module: config.templateModule,
+            templateFooter: '}]); hawtioPluginLoader.addModule("' + config.templateModule + '");'
+        }))
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task('concat', ['template'], function() {
-  return gulp.src(['compiled.js', 'templates.js'])
-    .pipe(plugins.concat(config.js))
-    .pipe(gulp.dest(config.dist));
+gulp.task('concat', ['template'], function () {
+    var gZipSize = size(gZippedSizeOptions);
+
+    return gulp.src(['compiled.js', 'templates.js'])
+        .pipe(plugins.concat(config.js))
+        .pipe(gulp.dest(config.dist))
+        .pipe(size(normalSizeOptions))
+        .pipe(gZipSize);
 });
 
-gulp.task('clean', ['concat'], function() {
-  return gulp.src(['templates.js', 'compiled.js'], { read: false })
-    .pipe(plugins.clean());
+gulp.task('clean', ['concat'], function () {
+    return gulp.src(['templates.js', 'compiled.js'], {read: false})
+        .pipe(plugins.clean());
 });
 
-gulp.task('watch', ['build'], function() {
-  plugins.watch(['libs/**/*.js', 'libs/**/*.css', 'index.html', config.dist + '/' + config.js], function() {
-    gulp.start('reload');
-  });
-  plugins.watch(['libs/**/*.d.ts', config.ts, config.templates], function() {
-    gulp.start(['tslint-watch', 'tsc', 'template', 'concat', 'clean']);
-  });
+gulp.task('watch', ['build'], function () {
+    plugins.watch(['libs/**/*.js', 'libs/**/*.css', 'index.html', config.dist + '/' + config.js], function () {
+        gulp.start('reload');
+    });
+    plugins.watch(['libs/**/*.d.ts', config.ts, config.templates], function () {
+        gulp.start(['tslint-watch', 'tsc', 'template', 'concat', 'clean']);
+    });
 });
 
-gulp.task('connect', ['watch'], function() {
-  plugins.connect.server({
-    root: '.',
-    livereload: true,
-    port: 2772,
-    fallback: 'index.html'
-  });
+gulp.task('connect', ['watch'], function () {
+    plugins.connect.server({
+        root: '.',
+        livereload: true,
+        port: 2772,
+        fallback: 'index.html'
+    });
 });
 
-gulp.task('reload', function() {
-  gulp.src('.')
-    .pipe(plugins.connect.reload());
+gulp.task('reload', function () {
+    gulp.src('.')
+        .pipe(plugins.connect.reload());
 });
 
 gulp.task('build', ['bower', 'path-adjust', 'tslint', 'tsc', 'template', 'concat', 'clean']);
