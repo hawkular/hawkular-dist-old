2c37b035485a19f66e5bcef30197e56b64b469f8
John Mazzitelli <mazz@redhat.com>
2015-07-10 17:46:12 -0400

    HAWKULAR-437 starting the feed-comm infrastructure

---
+++ modules/feed-comm/src/main/java/org/hawkular/feedcomm/ws/FeedCommWebSocket.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.hawkular.feedcomm.ws;
+
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/")
+public class FeedCommWebSocket {
+
+    @OnOpen
+    public void init(Session client) {
+        // TODO
+    }
+
+    @OnMessage
+    public String echo(String message, Session client) {
+        // TODO what to do?
+        return message;
+    }
+
+    @OnClose
+    public void clientClose(Session client, CloseReason reason) {
+        // TODO
+    }
+}
