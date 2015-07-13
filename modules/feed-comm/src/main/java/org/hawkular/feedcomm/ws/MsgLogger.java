fd0841d633ea845ea90924935fed32cb8c92e534
John Mazzitelli <mazz@redhat.com>
2015-07-13 11:41:18 -0400

    fix logging. add readme with simple client

--- modules/feed-comm/src/main/java/org/hawkular/feedcomm/ws/MsgLogger.java
+++ modules/feed-comm/src/main/java/org/hawkular/feedcomm/ws/MsgLogger.java
@@ -18,9 +18,9 @@ package org.hawkular.feedcomm.ws;
 
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.Logger;
-import org.jboss.logging.annotations.LogMessage;
-import org.jboss.logging.annotations.Message;
-import org.jboss.logging.annotations.MessageLogger;
+import org.jboss.logging.LogMessage;
+import org.jboss.logging.Message;
+import org.jboss.logging.MessageLogger;
 import org.jboss.logging.annotations.ValidIdRange;
 
 @MessageLogger(projectCode = "HAWKFEEDCOMM")
