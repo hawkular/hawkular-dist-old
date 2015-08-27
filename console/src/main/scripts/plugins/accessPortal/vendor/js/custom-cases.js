9039d423b5a81fa7d73fc9ba8ca839d7d7c22b56
Juraci Paixão Kröhling <juraci@kroehling.de>
2015-08-27 14:18:11 +0200

    HAWKULAR-556 - Attach Hawkular's JDR to case.

--- console/src/main/scripts/plugins/accessPortal/vendor/js/custom-cases.js
+++ console/src/main/scripts/plugins/accessPortal/vendor/js/custom-cases.js
@@ -1,10 +1,12 @@
+var attachmentsRequest = 'hawkular-redhat-access-integration-backend/attachments';
+
 angular.module('RedhatAccess.JON', ['RedhatAccess.cases'])
 .controller('customCase', ['$scope', 'securityService', 'NEW_DEFAULTS', '$location', '$http', function($scope, securityService, NEW_DEFAULTS , $location, $http) {
   NEW_DEFAULTS.product = "Red Hat JBoss Operations Network";
   NEW_DEFAULTS.version = "3.3.0";
 
   var params = $location.search();
-  
+
   // handle support case for managed resource
   if ($location.path().indexOf('/resource-case') >= 0) {
     NEW_DEFAULTS.product = params.product;
@@ -16,3 +18,19 @@ angular.module('RedhatAccess.JON', ['RedhatAccess.cases'])
      securityService.validateLogin(true);
    };
 }]);
+
+angular.module('RedhatAccess.cases')
+  .controller('BackEndAttachmentsCtrl', ['$scope', 'TreeViewSelectorData', 'AttachmentsService',
+    function ($scope, TreeViewSelectorData, AttachmentsService) {
+      $scope.name = 'Attachments';
+      $scope.attachmentTree = [];
+      TreeViewSelectorData.getTree(attachmentsRequest).then(
+        function (tree) {
+          $scope.attachmentTree = tree;
+          AttachmentsService.updateBackEndAttachments(tree);
+        },
+        function () {
+          console.log('Unable to get tree data');
+        });
+    }
+  ]);
